371ff9f9485e87737b332bf8cdc0f6b56c85a398
wmshort <w.short@exeter.ac.uk>
2020-04-15 11:58:13 +0100

    minor bug fixes, bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setup(
     name="greekwordnet",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(),
     url="https://greekwordnet.chs.harvard.edu",
     license="GNU General Public License v3.0",
@@ -20,5 +20,5 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=requirements,
-    test_suite="tests",
+    test_suite="",
 )
